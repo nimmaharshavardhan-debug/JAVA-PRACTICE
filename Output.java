c6b15cd4640e03d63701774ff687657ca9439ca6
Harsha Vardhan N <nimmaharshavardhann@gmail.com>
2025-12-21 07:31:29 +0530

    Add files via upload

---
+++ Output.java
@@ -0,0 +1,10 @@
+
+public class Output {
+    // to display out put 
+    public static void main(String[] args){
+        System.out.print("hello");
+        System.out.println("Hello world");
+        System.out.println("Hello\nworld ");
+    }
+    
+}
