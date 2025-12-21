48f158dc7183552e769e526668a5be642ce9f1cb
Harsha Vardhan N <nimmaharshavardhann@gmail.com>
2025-12-21 07:32:19 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    // primitive Data Types 
+    public static void main(String[] args){
+        String name = "Harsha Vardhan N ";
+        int age = 19;
+        double height = 5.6;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+
+
+        
+    }
+    
+}
