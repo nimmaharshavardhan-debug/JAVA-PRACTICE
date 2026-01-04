94550eab9d7c7583286458e167a0fefc0adc80fe
Harsha Vardhan N <nimmaharshavardhann@gmail.com>
2026-01-04 15:15:14 +0530

    Add files via upload

---
+++ CurlyBraces.java
@@ -0,0 +1,11 @@
+public class CurlyBraces{
+    public static void main(String [] args){
+        // Curly Braces and indentation in java
+        // In java curly braces are used to define the block of code
+        {
+            System.out.println("This is the inside the block");
+        }
+        System.out.println("This is the out side the block ");
+
+    }
+}
