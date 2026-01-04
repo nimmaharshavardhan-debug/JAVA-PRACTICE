0bb2ed7b28e7d1a2a04e43b2105341b800ba5b17
Harsha Vardhan N <nimmaharshavardhann@gmail.com>
2026-01-04 15:14:40 +0530

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,13 @@
+public class Comments{
+    public static void main(String [] args){
+        // declartaion of variables "This is a single line comments "
+        int age = 10;
+
+        /* To diplay the out we use the println function to show
+         the out put This is a multiple line comments */
+
+        System.out.println("AGE : "+age);
+
+
+    }
+}
