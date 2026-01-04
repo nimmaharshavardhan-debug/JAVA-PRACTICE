af960ada62b48a56a789f7af7c7c3f48df3cfe9c
Harsha Vardhan N <nimmaharshavardhann@gmail.com>
2026-01-04 15:16:22 +0530

    Add files via upload

---
+++ outPut.java
@@ -0,0 +1,6 @@
+public class outPut{
+    public static void main(String[] args){
+        //Output
+        System.out.println("Hello,My name Harsha Vardhan N ");//println() is function displays the out put
+    }
+}
